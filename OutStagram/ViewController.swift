84a22456af390489f824583806668ff97f83aa14
yu-jinny <151537809+yu-jinny@users.noreply.github.com>
2024-01-27 01:49:07 +0900

    Initial Commit

---
+++ OutStagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OutStagram
+//
+//  Created by t2023-m0028 on 1/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
